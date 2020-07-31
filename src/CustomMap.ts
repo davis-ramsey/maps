6b95545674eff5b9d96c187bc59de5786f62b2d0
Davis Ramsey <davis.ramsey@gmail.com>
2020-07-31 15:51:17 -0400

    finished!

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,12 +1,37 @@
+import { Company } from './Company';
+import { User } from './User';
+
+export interface Mappable {
+	location: {
+		lat: number;
+		lng: number;
+	};
+	markerContent(): string;
+	color: string;
+}
+
 export class CustomMap {
 	private googleMap: google.maps.Map;
-	constructor() {
-		this.googleMap = new google.maps.Map(document.querySelector('#map'), {
+	constructor(divID: string) {
+		this.googleMap = new google.maps.Map(document.querySelector(divID), {
 			center: {
 				lat: 0,
 				lng: 0
 			},
 			zoom: 1
+		});
+	}
+	addMarker(mappable: Mappable): void {
+		const marker = new google.maps.Marker({
+			map: this.googleMap,
+			position: {
+				lat: mappable.location.lat,
+				lng: mappable.location.lng
+			}
+		});
+		marker.addListener('click', () => {
+			const infoWindow = new google.maps.InfoWindow({ content: mappable.markerContent() });
+			infoWindow.open(this.googleMap, marker);
 		});
 	}
 }
